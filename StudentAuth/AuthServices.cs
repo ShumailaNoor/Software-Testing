ce89c4d63a3e31e937fa40c5cf093b11c6239bcb
ShumailaNoor <noorshumaila97@gmail.com>
2025-06-03 10:19:01 +0500

    Project Added

---
+++ StudentAuth/AuthServices.cs
@@ -0,0 +1,20 @@
+﻿namespace StudentAuth
+{
+    public class RegistrationService
+    {
+        public bool Register(User user)
+        {
+            if (string.IsNullOrWhiteSpace(user.username))
+            {
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(user.password) || user.password.Length < 6)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ StudentAuthTests/StudentAuthTests.cs
@@ -0,0 +1,36 @@
+namespace StudentAuth;
+
+public class Tests
+{
+    private RegistrationService service;
+
+    [SetUp]
+    public void Setup()
+    {
+        service = new RegistrationService();
+    }
+
+    [Test]
+    public void RegistrationWithValidUser_ReturnsTrue()
+    {
+        var newUser = new User { username = "Saira", password = "123456" };
+        var result = service.Register(newUser);
+        Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void Register_WithEmptyUsername_ReturnsFalse()
+    {
+        var user = new User { username = "", password = "pass123" };
+        var result = service.Register(user);
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void Register_WithShortPassword_ReturnsFalse()
+    {
+        var user = new User { username = "Ali", password = "" };
+        var result = service.Register(user);
+        Assert.IsFalse(result);
+    }
+}
